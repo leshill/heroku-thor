b71ec3fa0bab33f25277cfe90b21de7c4e841d5f
Les Hill <leshill@gmail.com>
2010-11-24 10:20:49 -0500

    Convert numbers in YAML config to strings for ENV

--- heroku_env.rb
+++ heroku_env.rb
@@ -2,6 +2,6 @@ heroku_config = File.expand_path('../heroku.yml', __FILE__)
 if File.exists?(heroku_config)
   config = YAML.load_file(heroku_config)
   config.fetch(Rails.env, {}).each do |key, value|
-    ENV[key.upcase] = value
+    ENV[key.upcase] = value.to_s
   end
 end
